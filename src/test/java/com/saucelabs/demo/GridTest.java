1e0bf5278ccf4f6cea74655bbfe751f419327ee4
Diego Molina <diemol@gmail.com>
2021-05-28 10:16:01 +0200

    Updating caps for Grid

--- src/test/java/com/saucelabs/demo/GridTest.java
+++ src/test/java/com/saucelabs/demo/GridTest.java
@@ -1,10 +1,9 @@
 package com.saucelabs.demo;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
@@ -43,7 +42,9 @@ http://localhost:16686/
   public void chromeTest() throws MalformedURLException, InterruptedException {
     URL gridUrl = new URL("http://localhost:4444");
     ChromeOptions options = new ChromeOptions();
-    options.setCapability("se:options", getSeOptions());
+    options.setCapability("se:recordVideo", true);
+    options.setCapability("se:timeZone", "US/Pacific");
+    options.setCapability("se:screenResolution", "1920x1080");
     RemoteWebDriver webDriver = new RemoteWebDriver(gridUrl, options);
     try {
       navigate(webDriver);
@@ -56,7 +57,9 @@ http://localhost:16686/
   public void firefoxTest() throws MalformedURLException, InterruptedException {
     URL gridUrl = new URL("http://localhost:4444");
     FirefoxOptions options = new FirefoxOptions();
-    options.setCapability("se:options", getSeOptions());
+    options.setCapability("se:recordVideo", true);
+    options.setCapability("se:timeZone", "US/Pacific");
+    options.setCapability("se:screenResolution", "1920x1080");
     RemoteWebDriver webDriver = new RemoteWebDriver(gridUrl, options);
     try {
       navigate(webDriver);
@@ -66,10 +69,12 @@ http://localhost:16686/
   }
 
   @Test
-  public void operaTest() throws MalformedURLException, InterruptedException {
+  public void edgeTest() throws MalformedURLException, InterruptedException {
     URL gridUrl = new URL("http://localhost:4444");
-    OperaOptions options = new OperaOptions();
-    options.setCapability("se:options", getSeOptions());
+    EdgeOptions options = new EdgeOptions();
+    options.setCapability("se:recordVideo", true);
+    options.setCapability("se:timeZone", "US/Pacific");
+    options.setCapability("se:screenResolution", "1920x1080");
     RemoteWebDriver webDriver = new RemoteWebDriver(gridUrl, options);
     try {
       navigate(webDriver);
@@ -79,7 +84,7 @@ http://localhost:16686/
   }
 
   private void navigate(RemoteWebDriver webDriver) throws InterruptedException {
-    long sleepLength = 500;
+    long sleepLength = 5000;
     webDriver.manage().window().maximize();
     webDriver.get("https://time.is/");
     Thread.sleep(sleepLength);
@@ -91,14 +96,6 @@ http://localhost:16686/
     Thread.sleep(sleepLength);
     webDriver.get("https://opensource.saucelabs.com/");
     Thread.sleep(sleepLength);
-  }
-
-  private MutableCapabilities getSeOptions() {
-    MutableCapabilities capabilities = new MutableCapabilities();
-    capabilities.setCapability("recordVideo", "true");
-    capabilities.setCapability("timeZone", "US/Pacific");
-    capabilities.setCapability("screenResolution", "1920x1080");
-    return capabilities;
   }
 
 }
